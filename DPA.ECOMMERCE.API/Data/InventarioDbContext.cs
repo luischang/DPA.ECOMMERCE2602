f83cd12f6fd692ff16f696d327df11d9c2ed2780
Luis Chang <luis.chang@outlook.com>
2026-09-20 12:45:47 -0500

    Agregar archivos de proyecto.

---
+++ DPA.ECOMMERCE.API/Data/InventarioDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DPA.ECOMMERCE.API.Data;
+
+public partial class InventarioDbContext : DbContext
+{
+    public InventarioDbContext()
+    {
+    }
+
+    public InventarioDbContext(DbContextOptions<InventarioDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Products> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=AO2302454;Database=InventarioDB;Integrated Security=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Products>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC07722E3316");
+
+            entity.Property(e => e.Description).HasMaxLength(50);
+            entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DPA.ECOMMERCE.API/Data/Products.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DPA.ECOMMERCE.API.Data;
+
+public partial class Products
+{
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal? UnitPrice { get; set; }
+}
