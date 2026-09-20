2c07ba1dbd957575bf6a54f8081a88d3c8bd119d
Luis Chang <luis.chang@outlook.com>
2026-09-20 13:53:37 -0500

    CRUD products

--- DPA.ECOMMERCE.API/Program.cs
+++ DPA.ECOMMERCE.API/Program.cs
@@ -1,6 +1,16 @@
+using DPA.ECOMMERCE.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("InventarioDbContext") ?? throw new InvalidOperationException("Connection string 'InventarioDbContext' not found.");
 
 // Add services to the container.
+var _config = builder.Configuration;
+var cnx = _config.GetConnectionString("DevConnection");
+builder
+.Services
+.AddDbContext<InventarioDbContext>
+(options => options.UseSqlServer(cnx));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
